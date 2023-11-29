aaddd7678377a26a68121af1e34fe9b8a395c353
FraRussotto <russottofrancesco1@gmail.com>
2023-11-29 20:14:07 +0100

    [edit] class condition in aside.blade

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -6,16 +6,16 @@
         </a>
 
 
-        <a href="{{ route('admin.home') }}">
+        <a href="{{ route('admin.home') }}" @class(['active' => Route::is('admin.home')])>
             <li class="nav-item">Dashboard</li>
         </a>
 
 
-        <a href="{{ route('admin.projects.index') }}">
+        <a href="{{ route('admin.projects.index') }}" @class(['active' => Route::is('admin.projects.index')])>
             <li class="nav-item">Projects list</li>
         </a>
 
-        <a href="{{ route('admin.projects.create') }}">
+        <a href="{{ route('admin.projects.create') }}" @class(['active' => Route::is('admin.projects.create')])>
             <li class="nav-item">Add project</li>
         </a>
 
