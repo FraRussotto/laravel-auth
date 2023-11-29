81c8211464b5e3250b986ae33f45d96e2a19f530
FraRussotto <russottofrancesco1@gmail.com>
2023-11-29 19:10:07 +0100

    [modify] layout header + [add] button home

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header class="bg-dark">
     <nav class="navbar navbar-dark">
-        <div class="container-fluid d-flex justi">
-            <a class="navbar-brand" target="_blank" href="{{ route('home') }}">BOOLFOLIO</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" target="_blank" href="{{ route('home') }}"><i class="fa-solid fa-house"></i></a>
+            <h2 class="logo">BOOLFOLIO</h2>
             <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
                 @csrf
                 <button class="btn btn-light" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
